53d7f6e460129e73c2b68e05a78666cb727e33cb
Conrad Mearns <conrad.mearns@pnnl.gov>
2022-08-23 16:57:13 -0700

    Add initial service-worker astgro package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 import svelte from "@astrojs/svelte";
+import mdx from "@astrojs/mdx";
+import serviceWorker from 'astro-service-worker';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
-});
-
-// http://proxy01.pnl.gov:3128
-
-
-// cafile=C:\Users\mear593\cert\cacert.pem
-// https-proxy=http://proxy01.pnl.gov:3128/
-// proxy=http://proxy01.pnl.gov:3128/
+  integrations: [
+    svelte(),
+    mdx(),
+    serviceWorker(),
+  ]
+}); 
