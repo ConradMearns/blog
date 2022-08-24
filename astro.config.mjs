f333dbdd7aaf36fa1909eaa3cf9ec8926fc38802
Conrad Mearns <conrad.mearns@pnnl.gov>
2022-08-24 09:04:20 -0700

    Deployment action for GH

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://conrads.website',
   integrations: [svelte(), mdx(), compress()]
 });
