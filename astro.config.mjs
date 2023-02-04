e6384442821a596cb061a271ad18c0ec1f642488
Conrad Mearns <conradmearns@pm.me>
2023-02-03 22:38:44 -0800

    Adds another image test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,24 +4,24 @@ import mdx from "@astrojs/mdx";
 import compress from "astro-compress";
 import remarkMath from 'remark-math';
 import rehypeMathJaxSvg from 'rehype-mathjax';
+import image from "@astrojs/image";
+
+// https://astro.build/config
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://conrads.website',
-  integrations: [
-    svelte(),
-    mdx(),
-    compress(),
-  ],
+  integrations: [svelte(), mdx(), compress(), image()],
   vite: {
     ssr: {
-      noExternal: [ 'p5-svelte' ]
+      noExternal: ['p5-svelte']
     }
   },
   markdown: {
     extendDefaultPlugins: true,
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeMathJaxSvg],
-  },
-
+    rehypePlugins: [rehypeMathJaxSvg]
+  }
 });
