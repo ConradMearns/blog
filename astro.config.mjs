048bc61f37f7b0d14e96694910943c14caeaef4a
Conrad Mearns <conradmearns@pm.me>
2023-09-11 18:25:28 -0700

    Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,46 +1,61 @@
-import { defineConfig } from 'astro/config';
-import svelte from "@astrojs/svelte";
-import mdx from "@astrojs/mdx";
-import compress from "astro-compress";
-import remarkMath from 'remark-math';
-import rehypeMathJaxSvg from 'rehype-mathjax';
-import image from "@astrojs/image";
-import remarkToc from 'remark-toc';
-import remarkWikiLink from '@flowershow/remark-wiki-link';
+import { defineConfig } from "astro/config";
+// import mdx from "@astrojs/mdx";
+// import compress from "astro-compress";
+// import remarkMath from 'remark-math';
+// import rehypeMathJaxSvg from 'rehype-mathjax';
+// import image from "@astrojs/image";
+// import remarkToc from 'remark-toc';
+import remarkWikiLink from "@flowershow/remark-wiki-link";
 
+const pageUrlPathPrefix = "blog/notes/";
 
 //Https for local development
-import basicSsl from '@vitejs/plugin-basic-ssl'
+// import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // Custom remark plugins
-import { remarkReadingTime } from './src/remark/remark-reading-time.mjs';
+import { remarkReadingTime } from "./src/remark/remark-reading-time.mjs";
+import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-
 export default defineConfig({
-  site: 'https://conrads.website',
-  integrations: [svelte(), mdx(), compress(), image()],
-  vite: {
-    ssr: {
-      noExternal: ['p5-svelte'],
-      plugins: [ basicSsl() ],
-      server: {
-        https: true,
-      }
-    }
-  },
+  site: "https://conrads.website",
+  integrations: [
+    tailwind({
+      applyBaseStyles: true,
+    }),
+    svelte(),
+    mdx(),
+  ],
+  // vite: {
+  //   ssr: {
+  //     // noExternal: ['p5-svelte'],
+  //     // plugins: [ basicSsl() ],
+  //     server: {
+  //       https: true
+  //     }
+  //   }
+  // },
   markdown: {
     extendDefaultPlugins: true,
     remarkPlugins: [
-      remarkToc,
-      remarkMath,
+      //   remarkToc,
+      //   remarkMath,
       remarkWikiLink,
+      // wikiLinkPlugin,
+      // {
+      //   pathFormat: "obsidian-short",
+      //   // generate url of the linked page.
+      //   // here `slug` would be "Page Name" for wiki link [[Page Name]].
+      //   wikiLinkResolver: (slug) => [pageUrlPathPrefix + slug],
+      // },
       remarkReadingTime,
     ],
     rehypePlugins: [
-      rehypeMathJaxSvg,
+      // rehypeMathJaxSvg,
       // ['rehype-toc', { headings: ['h1', 'h2'] }],
     ],
     // remarkWikiLink: { markdownFolder: "/obsimg/" }, // doesn't work?
-  }
+  },
 });
--- shell.nix
+++ shell.nix
@@ -3,10 +3,18 @@ with import <nixpkgs> { };
 stdenv.mkDerivation {
   name = "node";
   buildInputs = [
-    nodejs
+    nodejs_latest
+    nodePackages.pnpm
     openssl
+    bun
+
+    # For cms-server
+    python39
+    python39Packages.poetry
   ];
   shellHook = ''
     export PATH="$PWD/node_modules/.bin/:$PATH"
   '';
+    # cms-server
+  LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.stdenv.cc.cc ];
 }
